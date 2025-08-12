7a905501fee975ab67b08cc3e0633bc594e5cd81
LordZerovx <paulohenri445@gmail.com>
2025-08-12 18:31:34 -0300

    Botão excluir adicionado e simbolos não aparecem mais na tela

--- Calculadora.js
+++ Calculadora.js
@@ -8,6 +8,8 @@ const botoes = document.querySelectorAll('.botoes-calc');
 
 const botaoLimpar = document.querySelector('.botoes-calc-apagar');
 
+const botaoExcluir = document.getElementById("botaoExcluir");
+
 function atualizarTela() {
     telaCalc.textContent = buffer;
 }
@@ -16,18 +18,33 @@ atualizarTela()
 botoes.forEach(botao => {
     botao.addEventListener('click', (event) => {
         event.stopPropagation();
-        const digito = event.target.textContent;
+        let digito = event.target.textContent.trim();
 
-        if (buffer === '0') {
-            buffer = digito;
-        } else {
-            buffer += digito;
+        if(digito === "←") return;
+
+
+        switch(digito){
+            case "=":
+            case "+":
+            case "−":
+            case "×":
+            case "÷":
+                return;
         }
 
+        if(buffer == '0') {
+            buffer = digito;
+        } else {
+            buffer += digito
+            buffer.length++;
+        }
+        
        atualizarTela()
         
     })
 })
+
+
 
 botaoLimpar.addEventListener('click', () => {
     event.stopPropagation();
@@ -37,16 +54,8 @@ botaoLimpar.addEventListener('click', () => {
     atualizarTela();
 })
 
-botaoExcluir.addEventListener('click', () => {
-   
-    if (buffer.length === 1) {
-        buffer = '0';
-    } else {
-        buffer = buffer.toString(0, buffer.length - 1);
-    }
+botaoExcluir.addEventListener('click', (event) => {
+    buffer = buffer.slice(0, -1) || "0";
     atualizarTela();
 });
 
-console.log(buffer);
-console.log(totalCalculado);
-console.log(operacaoAnterior);
